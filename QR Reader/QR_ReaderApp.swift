c2c96450e3f403cf38506f7485c01fbb2c73b2ab
Christopher Arias <christopher.arias.jr@gmail.com>
2023-06-20 15:19:00 -0700

    Initial Commit

---
+++ QR Reader/QR_ReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  QR_ReaderApp.swift
+//  QR Reader
+//
+//  Created by Christopher Arias on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct QR_ReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
